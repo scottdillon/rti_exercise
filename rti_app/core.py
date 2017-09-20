8ca2371b25d50750746344a9dbd2cee00df499a8
Scott Dillon <scott.dillon@gmail.com>
2017-09-20 12:50:18 -0400

    Working 2D Histogram on the hours worked data but it's hard to read since it's a lot of information and dense areas are saturated.

--- rti_app/core.py
+++ rti_app/core.py
@@ -1,3 +1,4 @@
+import numpy as np
 import plotly as plt
 from .resource import exercise_libs as el
 
@@ -62,22 +63,55 @@ def get_index_text():
 
 def hours_worked_plot():
     """
-
+    Creates a plot of hours worked for all ages. There should be a boxplot with a box
+    for each age and an average trace for the average for that age.
+    
+    DO NOT add a scatter object for each age value. There are 48k values and the javascript
+    takes forever on slow computers (i.e. my work laptop) Chrome profiled this process as 
+    taking 33s while the page took 2.5s to reload when the original hours/age scatter trace is
+    removed.
     :return:
     """
-    dataframe = data_processor.census_data
-    x = dataframe.loc[:, 'Age'].values
-    y = dataframe.loc[:, 'Hours Per Week'].values
+    hours_dataframe = data_processor.census_data[['Age','Hours Per Week']]
+    box_objs_list = return_box_plots_of_hours_worked(hours_dataframe)
+    x = hours_dataframe.loc[:, 'Age'].values
+    y = hours_dataframe.loc[:, 'Hours Per Week'].values
+    hours = plt.graph_objs.Histogram2dcontour(
+        x=x, y=y, name='density', ncontours=10,
+        colorscale='Hot', reversescale=True, showscale=False,
+        zmax=60,
+        nbinsx=80,
+        nbinsy=80,
+        zauto=False,
+        opacity=0.3
+
+        # autobinx=False, xbins = dict(start=16, end=100, size=3),
+        # autobiny=False, ybins = dict(start=0, end=100, size=3)
+    )
+    #el.HoursWorkedTrace(x=x, y=y)
     mean_groupby = data_processor.groupby(['Age'])
     agg_dict = {'Hours Per Week': 'mean'}
     mean_hours_worked = data_processor.aggregate_groupby(mean_groupby, agg_dict)
     mean_x = mean_hours_worked.index.values
     mean_y = mean_hours_worked['Hours Per Week'].values
-    hours_worked_trace = el.HoursWorkedTrace(x, y)
     mean_hours_worked_trace = el.AvgHoursWorkedTrace(mean_x, mean_y)
-    fig = el.HoursWorkedFigure([hours_worked_trace, mean_hours_worked_trace])
-    div = el.get_plotly_div_str(fig)
+    trace_list = box_objs_list.append(mean_hours_worked_trace)
+    fig = el.HoursWorkedFigure(data=[hours, mean_hours_worked_trace])   #.append(mean_hours_worked_trace))
+    div = el.get_plotly_div_str(figure_obj=fig)
     return div
+
+def return_box_plots_of_hours_worked(hours_df) -> list:
+    boxplot_traces = []
+    for age in hours_df['Age'].unique():
+
+        box = plt.graph_objs.Box()
+        box.x = age
+        box.name = age
+        box.boxpoints = False
+        box.y = hours_df.loc[hours_df['Age'] == age].loc[:,'Hours Per Week']
+        boxplot_traces.append(box)
+    print('len of boxplot traces is {}'.format(len(boxplot_traces)))
+    return boxplot_traces
 
 
 
