11b7c7e6416f5af35bbcc9fb53e045217222061b
Scott Dillon <scott.dillon@gmail.com>
2017-09-20 22:24:06 -0400

    imported the over_50k_country_origin function. Added this to the index route.

--- rti_app/views.py
+++ rti_app/views.py
@@ -1,8 +1,7 @@
-import os
 from rti_app import my_app
 from flask import render_template, request
-from .core import get_index_text, hours_worked_plot, histogram_hours_worked
-# from .resource import exercise_libs as el
+from .core import get_index_text, hours_worked_plot
+from .core import histogram_hours_worked, over_50k_country_origin
 
 
 @my_app.route('/')
@@ -15,11 +14,13 @@ def index():
     summary_html, html_over_50k = get_index_text()
     hours_worked_div = hours_worked_plot()
     histogram = histogram_hours_worked()
+    choropleth = over_50k_country_origin()
     return render_template('index.html',
                            summary_stats=summary_html,
                            over_50k_race_marr=html_over_50k,
                            hours_worked=hours_worked_div,
-                           histogram_hours_worked=histogram)
+                           histogram_hours_worked=histogram,
+                           map_over50k=choropleth)
 
 
 @my_app.route('/show_data')
