a219d6b3c8e6f4154ecc67f1359466944e837a86
Scott Dillon <scott.dillon@gmail.com>
2017-09-20 21:21:20 -0400

    linked in the histogram function from core.py to get histogram html to insert into template.

--- rti_app/views.py
+++ rti_app/views.py
@@ -1,7 +1,7 @@
 import os
 from rti_app import my_app
 from flask import render_template, request
-from .core import get_index_text, hours_worked_plot
+from .core import get_index_text, hours_worked_plot, histogram_hours_worked
 # from .resource import exercise_libs as el
 
 
@@ -14,10 +14,12 @@ def index():
     """
     summary_html, html_over_50k = get_index_text()
     hours_worked_div = hours_worked_plot()
+    histogram = histogram_hours_worked()
     return render_template('index.html',
                            summary_stats=summary_html,
                            over_50k_race_marr=html_over_50k,
-                           hours_worked=hours_worked_div)
+                           hours_worked=hours_worked_div,
+                           histogram_hours_worked=histogram)
 
 
 @my_app.route('/show_data')
