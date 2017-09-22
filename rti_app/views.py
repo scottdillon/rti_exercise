1c2df55c3e3499ed9c54727b106018ccd304828c
Scott Dillon <scott.dillon@gmail.com>
2017-09-22 12:19:22 -0400

    Redo teh flask imports here and all the imports from core.py. Easier to follow.

--- rti_app/views.py
+++ rti_app/views.py
@@ -1,9 +1,8 @@
+import flask
+
 from rti_app import my_app
-from flask import render_template, request
-from .core import get_index_text, hours_worked_plot
-from .core import histogram_hours_worked, over_50k_country_origin
-from .core import data_processor, chop_dataframe
-from .resource.exercise_libs import change_table_css_class
+import rti_app.core as core
+import rti_app.resource.exercise_libs as el
 
 
 @my_app.route('/')
@@ -14,11 +13,11 @@ def index():
 
     :return: renders the template and returns the html
     """
-    summary_html, html_over_50k = get_index_text()
-    hours_worked_div = hours_worked_plot()
-    histogram = histogram_hours_worked()
-    choropleth = over_50k_country_origin()
-    return render_template('index.html',
+    summary_html, html_over_50k = core.get_index_text()
+    hours_worked_div = core.hours_worked_plot()
+    histogram = core.histogram_hours_worked()
+    choropleth = core.over_50k_country_origin()
+    return flask.render_template('index.html',
                            title="RTI Exercise, Scott Dillon",
                            summary_stats=summary_html,
                            over_50k_race_marr=html_over_50k,
@@ -44,18 +43,18 @@ def show_data(page=1):
     """
     page_length = 25
 
-    chop_dataframe(data_processor, page_length)
+    core.chop_dataframe(core.data_processor, page_length)
 
-    if request.args.get('page'):
-        page = int(request.args.get('page'))
+    if flask.request.args.get('page'):
+        page = int(flask.request.args.get('page'))
 
     first_page = 1
-    last_page = len(data_processor.list_pages)
+    last_page = len(core.data_processor.list_pages)
     page = sorted([first_page, page, last_page])[1]
 
-    table_html = data_processor.list_pages[page - 1]
-    table_html = change_table_css_class(table_html, index=True)
-    return render_template('show_data.html',
+    table_html = core.data_processor.list_pages[page - 1]
+    table_html = el.change_table_css_class(table_html, index=True)
+    return flask.render_template('show_data.html',
                            title="RTI Exercise, Scott Dillon",
                            table_html=table_html,
                            page=page,
