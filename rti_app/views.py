6d3b6d14afc364f10947fa9157f484c3feb50745
Scott Dillon <scott.dillon@gmail.com>
2017-09-21 22:03:15 -0400

    Added comments and fixed the show_data function to make it a little more economized.

--- rti_app/views.py
+++ rti_app/views.py
@@ -5,6 +5,7 @@ from .core import histogram_hours_worked, over_50k_country_origin
 from .core import data_processor, chop_dataframe
 from .resource.exercise_libs import change_table_css_class
 
+
 @my_app.route('/')
 @my_app.route('/index')
 def index():
@@ -30,23 +31,27 @@ def index():
 @my_app.route('/show_data?page=<int:page>', methods=['GET', 'POST'])
 def show_data(page=1):
     """
+    Displays the paginated view of the data. There are a few things going
+    on here so lets go through them.
 
-    :return:
+    First, set the records to show for each page.
+    Then, chop the large dataframe up if it isn't already.
+    Get the page number from the query string.
+    Make sure the page number is within the bounds of our page number
+        which are also the indices of the data_processor.list_pages
+        which is our list of smaller size databases.
+    :return: render the template with our templated stuff in it.
     """
     page_length = 25
 
     chop_dataframe(data_processor, page_length)
 
-
     if request.args.get('page'):
         page = int(request.args.get('page'))
+
     first_page = 1
     last_page = len(data_processor.list_pages)
-
-    if page < first_page:
-        page = 1
-    if page > last_page:
-        page = last_page
+    page = sorted([first_page, page, last_page])[1]
 
     table_html = data_processor.list_pages[page - 1]
     table_html = change_table_css_class(table_html, index=True)
